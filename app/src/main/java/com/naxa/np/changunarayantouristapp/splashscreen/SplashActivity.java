16131a36ce82eae6de33e4e864f76b3fecb34b19
samirdangal <samirdangal@gmail.com>
2019-06-10 14:07:55 +0545

    [#1] walk through screen layout created

---
+++ app/src/main/java/com/naxa/np/changunarayantouristapp/splashscreen/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.naxa.np.changunarayantouristapp.splashscreen;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.naxa.np.changunarayantouristapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
