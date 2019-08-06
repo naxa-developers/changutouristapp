2081e8a041b216f9164d50eb6051f9d1de4653d4
samirdangal <samirdangal@gmail.com>
2019-08-06 15:30:50 +0545

    [#212] abobut us UI and content update

--- app/src/main/java/com/naxa/np/changunarayantouristapp/common/AboutUsActivity.java
+++ app/src/main/java/com/naxa/np/changunarayantouristapp/common/AboutUsActivity.java
@@ -3,6 +3,7 @@ package com.naxa.np.changunarayantouristapp.common;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,6 +11,9 @@ import android.webkit.WebViewClient;
 import com.franmontiel.localechanger.LocaleChanger;
 import com.naxa.np.changunarayantouristapp.R;
 import com.naxa.np.changunarayantouristapp.utils.Constant;
+import com.naxa.np.changunarayantouristapp.utils.SharedPreferenceUtils;
+
+import java.util.Locale;
 
 public class AboutUsActivity extends BaseActivity {
 
@@ -31,17 +35,48 @@ public class AboutUsActivity extends BaseActivity {
         web = findViewById(R.id.webViewAboutUs);
 
         setupWebView(Constant.getAboutUsDemoContent());
+        setupWebView(Constant.getAboutUsDemoContentEnglish());
+
+        setUpWebAboutUsWebContent();
+
+    }
+
+    private void setUpWebAboutUsWebContent() {
+        String alias = "en";
+        String appLanguage = SharedPreferenceUtils.getInstance(ChangunarayanTouristApp.getInstance()).getStringValue(Constant.SharedPrefKey.KEY_SELECTED_APP_LANGUAGE, null);
+        if (!TextUtils.isEmpty(appLanguage)) {
+            alias = appLanguage;
+        }
+
+        switch (alias) {
+            case "nep":
+                setupWebView(Constant.getAboutUsDemoContent());
+                break;
+
+            case "en":
+                setupWebView(Constant.getAboutUsDemoContentEnglish());
+                break;
+
+            case "ch":
+                setupWebView(Constant.getAboutUsDemoContentEnglish());
+                break;
+
+            default:
+                setupWebView(Constant.getAboutUsDemoContentEnglish());
+                break;
+
+        }
+
 
     }
 
     private void setupWebView(String aboutUs) {
         web.setWebViewClient(new myWebClient());
         web.getSettings().setJavaScriptEnabled(true);
-        web.loadDataWithBaseURL(null,aboutUs,"text/html", "utf-8", null);
+        web.loadDataWithBaseURL(null, aboutUs, "text/html", "utf-8", null);
     }
 
-    public class myWebClient extends WebViewClient
-    {
+    public class myWebClient extends WebViewClient {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             // TODO Auto-generated method stub
@@ -67,8 +102,7 @@ public class AboutUsActivity extends BaseActivity {
 
     // To handle "Back" key press event for WebView to go back to previous screen.
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_BACK) && web.canGoBack()) {
             web.goBack();
             return true;
